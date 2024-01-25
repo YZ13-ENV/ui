11320de690cc81de25b362e2025d682f03c4324b
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2024-01-25 14:19:21 +0500

    fix types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 "use client";
 import './globals.css'
 export type { MenuMapProps } from './types/common'
-
 export { projects } from './const/projects'
 export { Avatar, ProjectsGrid } from './components/shared'
 export { UserCircle } from "./components/widgets/user-circle";
